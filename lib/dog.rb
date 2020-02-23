4edbafd97cc0b8aa865d4bf61da207a3626f5b59
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-23 01:10:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+  def name
+  @name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+
+  
+  def self.print_all
+  @all.each { |x| puts x }
+end
+print_all
+ end
+ 
+  
+    
+  def self.clear_all
+    
+  end
+
+  
+end
